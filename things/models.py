9b35222cc55d239e88a2b5907785bbde6f9f717d
mmmmie <4mmmmie@gmail.com>
2023-10-07 10:40:00 +0100

    Change max length for description in Thing to 120

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.core import validators
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=100, unique=False, blank=True)
+    description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique = False, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)])
